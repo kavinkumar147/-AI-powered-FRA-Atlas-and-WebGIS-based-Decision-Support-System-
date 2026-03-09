a2ef8e141f42099561b8818a29bf4db153d46056
KAVINKUMAR G <kaving147@gmail.com>
2026-03-09 23:24:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,60 @@ var map = L.map('map').setView([20.5937, 78.9629], 5);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-// Madhya Pradesh FRA claim
-L.marker([23.2599, 77.4126])
-.addTo(map)
-.bindPopup("FRA Claim - Madhya Pradesh");
+/* ---------- FRA Status Icons ---------- */
 
-// Odisha FRA claim
-L.marker([20.2961, 85.8245])
-.addTo(map)
-.bindPopup("FRA Claim - Odisha");
+var approvedIcon = new L.Icon({
+iconUrl:'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+iconSize:[32,32]
+});
 
-// Telangana FRA claim
-L.marker([17.3850, 78.4867])
+var pendingIcon = new L.Icon({
+iconUrl:'https://maps.google.com/mapfiles/ms/icons/orange-dot.png',
+iconSize:[32,32]
+});
+
+var rejectedIcon = new L.Icon({
+iconUrl:'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+iconSize:[32,32]
+});
+
+/* ---------- FRA Claim Markers ---------- */
+
+// Madhya Pradesh
+L.marker([23.2599, 77.4126], {icon: approvedIcon})
 .addTo(map)
-.bindPopup("FRA Claim - Telangana");
+.bindPopup("Approved FRA Claim - Madhya Pradesh");
+
+// Odisha
+L.marker([20.2961, 85.8245], {icon: pendingIcon})
+.addTo(map)
+.bindPopup("Pending FRA Claim - Odisha");
+
+// Telangana
+L.marker([17.3850, 78.4867], {icon: rejectedIcon})
+.addTo(map)
+.bindPopup("Rejected FRA Claim - Telangana");
+
+/* ---------- Load State Layer ---------- */
+
+fetch("https://raw.githubusercontent.com/geohacker/india/master/state/india_telengana.geojson")
+.then(function(response){
+return response.json();
+})
+.then(function(data){
+
+L.geoJSON(data, {
+style: {
+color: "darkgreen",
+weight: 2,
+fillColor: "lightgreen",
+fillOpacity: 0.3
+},
+
+onEachFeature: function (feature, layer) {
+layer.bindPopup("State: " + feature.properties.NAME_1);
+}
+
+}).addTo(map);
+
+});
